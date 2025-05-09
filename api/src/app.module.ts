483ae3fb7f86dc621fa17e78e8486e67e0c3f7dd
alkaline1024 <earthza991@hotmail.com>
2025-05-09 17:42:48 +0700

    feat(api): create exception module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, LoggerModule],
+  imports: [EnvironmentConfigModule, LoggerModule, ExceptionsModule],
   controllers: [],
   providers: [],
 })
