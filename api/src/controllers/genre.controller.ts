8d6af07b9987685e6b34c12642191efc72b4c40e
alkaline1024 <earthza991@hotmail.com>
2025-05-11 18:06:23 +0700

    feat(api): change genres base url

--- api/src/controllers/genre.controller.ts
+++ api/src/controllers/genre.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { GenreUseCases } from '../use-cases/genre/genre-use-case.service';
 
-@Controller('genre')
+@Controller('genres')
 export class GenreController {
   constructor(private readonly genreUseCase: GenreUseCases) {}
 
