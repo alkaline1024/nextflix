1df58e0cd60c24f45c97dda1ad4dca1500f26b00
alkaline1024 <earthza991@hotmail.com>
2025-05-11 08:55:49 +0700

    feat(web): create infrastructure repository

---
+++ web/src/infrastructure/repositories/movie.repository.ts
@@ -0,0 +1,30 @@
+import { Movie } from "@/entities/models/movie";
+import { MovieRepository } from "@/application/repositories/movie.repository.interface";
+
+export class MovieApiRepository implements MovieRepository {
+  private baseUrl = "http://localhost:8080/movies";
+
+  async getPopular(): Promise<Movie[]> {
+    const res = await fetch(`${this.baseUrl}/popular`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getNowPlaying(): Promise<Movie[]> {
+    const res = await fetch(`${this.baseUrl}/now_playing`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getTopRated(): Promise<Movie[]> {
+    const res = await fetch(`${this.baseUrl}/top_rated`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getUpcoming(): Promise<Movie[]> {
+    const res = await fetch(`${this.baseUrl}/upcoming`);
+    const data = await res.json();
+    return data.results;
+  }
+}
---
+++ web/src/infrastructure/repositories/tv.repository.ts
@@ -0,0 +1,30 @@
+import { Tv } from "@/entities/models/tv";
+import { TvRepository } from "@/application/repositories/tv.repository.interface";
+
+export class TvApiRepository implements TvRepository {
+  private baseUrl = "http://localhost:8080/tv";
+
+  async getPopular(): Promise<Tv[]> {
+    const res = await fetch(`${this.baseUrl}/popular`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getAiringToday(): Promise<Tv[]> {
+    const res = await fetch(`${this.baseUrl}/airing_today`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getOnTheAir(): Promise<Tv[]> {
+    const res = await fetch(`${this.baseUrl}/on_the_air`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async getTopRated(): Promise<Tv[]> {
+    const res = await fetch(`${this.baseUrl}/top_rated`);
+    const data = await res.json();
+    return data.results;
+  }
+}
